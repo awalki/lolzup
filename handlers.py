ce74eae7fefe9926d7c51ee5334cd8201086b709
awalki <je3nsoff@icloud.com>
2025-09-13 17:10:28 +0300

    make sure no one can use your bot

--- handlers.py
+++ handlers.py
@@ -1,13 +1,20 @@
 from aiogram import Router
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, BaseFilter
 from aiogram.types import Message
 from aiogram_dialog import DialogManager, StartMode, ShowMode
 
 from dialogs import MainMenuSG
+from settings import settings
+
+
+class IsAdmin(BaseFilter):
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id == settings.admin_id
+
 
 router = Router()
 
 
-@router.message(CommandStart())
+@router.message(CommandStart(), IsAdmin())
 async def start(message: Message, dialog_manager: DialogManager):
     await dialog_manager.start(MainMenuSG.main, mode=StartMode.RESET_STACK, show_mode=ShowMode.DELETE_AND_SEND)
