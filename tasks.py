d0320f8ff0d08dea0162d03b205959fcef6d9a96
awalki <je3nsoff@icloud.com>
2025-09-13 22:50:39 +0300

    wrap rerun_bump with try except

--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+import asyncio
 import datetime
 import logging
 
@@ -22,22 +23,27 @@ async def bump_task(thread_id: str, bot: Bot = TaskiqDepends()) -> None:
 
 
 async def rerun_bump(thread_id: str):
-    logging.info(f"Задача запущена/перезапущена. Айди: {thread_id}")
+    try:
+        logging.info(f"Задача запущена/перезапущена. Айди: {thread_id}")
 
-    thread = await forum.threads.get(thread_id)
+        thread = await forum.threads.get(thread_id)
 
-    data = thread.json()
-    next_bump_timestamp = data["thread"]["permissions"]["bump"]["next_available_time"]
+        data = thread.json()
+        next_bump_timestamp = data["thread"]["permissions"]["bump"]["next_available_time"]
 
-    if next_bump_timestamp is None:
-        logging.info("Тему можно поднять сейчас, поднимаем!")
+        if next_bump_timestamp is None:
+            logging.info("Тему можно поднять сейчас, поднимаем!")
 
-        await bump_task.kiq(str(thread_id))
-    else:
-        logging.info("Тема в КД, ждем")
-        eta = datetime.datetime.fromtimestamp(next_bump_timestamp) + datetime.timedelta(seconds=15)
+            await bump_task.kiq(str(thread_id))
+        else:
+            logging.info("Тема в КД, ждем")
+            eta = datetime.datetime.fromtimestamp(next_bump_timestamp) + datetime.timedelta(seconds=15)
 
-        await bump_task.kicker().with_schedule_id(str(thread_id)).schedule_by_time(
-            redis_source,
-            eta,
-        )
+            await bump_task.kicker().with_schedule_id(str(thread_id)).schedule_by_time(
+                redis_source,
+                eta,
+            )
+    except Exception as e:
+        logging.error("Произошла непредвиденная ошибка при попытке запуска таски, пробуем еще раз через 15 секунд", e)
+        await asyncio.sleep(15)
+        await rerun_bump(thread_id)
